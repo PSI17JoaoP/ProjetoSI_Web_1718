23aa74dc6e8e9ff34ed5dc3ccdcb191a9ebd38d2
Leonardo <leonardo@MasterLeo>
2017-10-28 17:57:01 +0100

    Design Perfil de user (concluído)

--- frontend/views/user/conta.php
+++ frontend/views/user/conta.php
@@ -1,3 +1,95 @@
 <?php
 /* @var $this yii\web\View */
 ?>
+
+<div class="col-12 col-md-8">
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            Dados de utilizador
+        </div>
+        
+        <div class="panel-body">
+            <!-- TEMP - Talvez use widgets -->
+            <div class="row" style="padding-bottom: 5%">
+                <div class="col-md-4" >
+                    <img src="../web/assets/cfa4ff67/img/avatar.png"  width="125px" height="125px" >
+                    <button class="btn btn-default btn-sm">Escolher Imagem...</button>
+                </div>
+
+                <div class="col-md-8">
+                    <label for="nomeUser">Nome:</label>
+                    <input type="text" class="form-control" id="nomeUser">
+
+                    <label for="telefoneUser">Nº Telemóvel:</label>
+                    <input type="text" class="form-control" id="telefoneUser">
+
+                    <label for="emailUser">Email:</label>
+                    <input type="email" class="form-control" id="emailUser">
+
+                    <label for="regiaoUser">Região:</label>
+                    <select class="form-control" id="regiaoUser">
+                            <option value="" disabled selected>Distrito</option>
+                            <option value="Aveiro">Aveiro</option>
+                            <option value="Beja">Beja</option>
+                            <option value="Braga">Braga</option>
+                            <option value="Bragança">Bragança</option>
+                            <option value="Castelo Branco">Castelo Branco</option>
+                            <option value="Coimbra">Coimbra</option>
+                            <option value="Évora">Évora</option>
+                            <option value="Faro">Faro</option>
+                            <option value="Guarda">Guarda</option>
+                            <option value="Leiria">Leiria</option>
+                            <option value="Lisboa">Lisboa</option>
+                            <option value="Portalegre">Portalegre</option>
+                            <option value="Porto">Porto</option>
+                            <option value="Santarém">Santarém</option>
+                            <option value="Setúbal">Setúbal</option>
+                            <option value="Viana do Castelo">Viana do Castelo</option>
+                            <option value="Vila Real">Vila Real</option>
+                            <option value="Viseu">Viseu</option>
+                            <option value="Açores">Açores</option>
+                            <option value="Madeira">Madeira</option>
+                        </select>
+                </div>
+                
+            </div>
+
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            Categoria(s) preferida(s)
+                        </div>
+                        <div class="panel-body">
+                            <div class="checkbox">
+                                <label><input type="checkbox" value="brinquedos">Brinquedos</label>
+                            </div>
+                            <div class="checkbox">
+                                <label><input type="checkbox" value="jogos">Jogos</label>
+                            </div>
+                            <div class="checkbox">
+                                <label><input type="checkbox" value="eletronica">Eletrónica</label>
+                            </div>
+                            <div class="checkbox">
+                                <label><input type="checkbox" value="computadores">Computadores</label>
+                            </div>
+                            <div class="checkbox">
+                                <label><input type="checkbox" value="smartphones">Smartphones</label>
+                            </div>
+                            <div class="checkbox">
+                                <label><input type="checkbox" value="livros">Livros</label>
+                            </div>
+                            <div class="checkbox">
+                                <label><input type="checkbox" value="roupa">Roupa</label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="panel-footer" align="right">
+            <button class="btn btn-default btn-lg">Concluído</button>
+        </div>
+    </div>
+</div>
