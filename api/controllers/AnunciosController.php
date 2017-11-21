741e146e2f35c51fca896a05b7a039d28238e8a9
Leonardo <leonardo@MasterLeo>
2017-11-21 16:05:25 +0000

    Módulo API criado

---
+++ api/controllers/AnunciosController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace api\controllers;
+
+use yii\rest\ActiveController;
+
+class AnunciosController extends ActiveController
+{
+    public $modelClass = 'common\models\Anuncio';
+
+}
