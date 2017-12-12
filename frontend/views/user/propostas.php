2caf2a5e1fdadd50c230a3525edb7c49789ff872
Leonardo <leonardo@MasterLeo>
2017-12-12 09:51:44 +0000

    User>Propostas design alterado

--- frontend/views/user/propostas.php
+++ frontend/views/user/propostas.php
@@ -4,13 +4,25 @@
  */
 
 use yii\helpers\Html;
+use yii\helpers\Url;
+
 
 $this->title = 'Propostas Recebidas';
 ?>
 
+
+<?= $this->renderAjax('//modals/modal',[
+        'header' => "Detalhes",
+        'backdrop' => 'true',
+        'keyboard' => 'true',
+        'content' => '//modals/proposta',
+        'options' => [
+        ],
+    ]) ?>
+
 <div class="col-12 col-md-8">
     <div class="panel panel-default">
-        <div class="panel-body">
+        <div class="panel-body anuncio-detalhes">
 
             <?php foreach($propostas as $dados) { ?>
 
@@ -19,64 +31,43 @@ $this->title = 'Propostas Recebidas';
                         <div class="panel panel-default">
                             <div class="panel-body">
                                 <div class="row">
-                                    <div class="col-md-4">
+                                    <div class="col-md-6">
                                         <p style="margin-top: 12px; margin-left: 5px"><?= Html::encode($dados[1][0]->nome) ?></p>
                                     </div>
 
-                                    <div class="col-md-4">
+                                    <div class="col-md-6">
                                         <span class="pull-right">
-                                            <?= Html::a('Detalhes', '#', ['class' => 'btn btn-primary view_model'])?>
+                                            <?= Html::a('Detalhes', 'javascript:', [
+                                                'class' => 'btn btn-primary view_model',
+                                                'data-detail' => Url::toRoute(['anuncio/detalhes']), 
+                                                'data-id' => $dados[0]->id_anuncio
+                                                ])?>
+                                           
+                                            <?= Html::a('Aceitar', ['proposta/update', 'propostaID' => $dados[0]->id], [
+                                                'class' => 'btn btn-success',
+                                                'data' => [
+                                                    'method' => 'post',
+                                                    'params' => [
+                                                        'estado' => 'ACEITE',
+                                                    ],
+                                                ]
+                                            ]); ?>
+
+                                            <?= Html::a('Recusar', ['proposta/update', 'propostaID' => $dados[0]->id], [
+                                                'class' => 'btn btn-danger',
+                                                'data' => [
+                                                    'method' => 'post',
+                                                    'params' => [
+                                                        'estado' => 'RECUSADO',
+                                                    ],
+                                                ]
+                                            ]); ?>
                                         </span>
-                                    </div>
-
-                                    <div class="col-md-4">
-                                        <div class="row">
-                                            <div class="col-md-6">
-                                                <span class="pull-right">
-                                                    <?= Html::a('Aceitar', ['proposta/update', 'propostaID' => $dados[0]->id], [
-                                                        'class' => 'btn btn-success',
-                                                        'style' => 'margin-left: 17px; margin-top: 13px',
-                                                        'data' => [
-                                                            'method' => 'post',
-                                                            'params' => [
-                                                                'estado' => 'ACEITE',
-                                                            ],
-                                                        ]
-                                                    ]); ?>
-                                                </span>
-                                            </div>
-
-                                            <div class="col-md-6">
-                                                <span class="pull-right">
-                                                    <?= Html::a('Recusar', ['proposta/update', 'propostaID' => $dados[0]->id], [
-                                                        'class' => 'btn btn-danger',
-                                                        'style' => 'margin-left: 17px; margin-top: 13px',
-                                                        'data' => [
-                                                            'method' => 'post',
-                                                            'params' => [
-                                                                'estado' => 'RECUSADO',
-                                                            ],
-                                                        ]
-                                                    ]); ?>
-                                                </span>
-                                            </div>
-                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-
-                    <?= $this->renderAjax('//modals/modal',[
-                        'header' => $dados[1][0]->nome,
-                        'backdrop' => 'true',
-                        'keyboard' => 'true',
-                        'content' => '//modals/proposta',
-                        'options' => [
-                            'model' => $dados[0],
-                            'categorias' => $dados[1],
-                        ],
-                    ]) ?>
 
                 </div>
         <?php } ?>
