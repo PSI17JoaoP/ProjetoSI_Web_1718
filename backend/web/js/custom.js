567d2bac4811971689633beccf6fab93434e14cb
João P <2160453@my.ipleiria.pt>
2017-12-28 14:09:53 +0000

    Pequena correção na parte dos users e remoção de alguns espaço no js para ser mais fácil

--- backend/web/js/custom.js
+++ backend/web/js/custom.js
@@ -9,8 +9,8 @@ $(".rowListaUsers").click(function()
             type: 'json',
             data: 
                 {
-                    "id" : id,
-                },
+                    "id" : id
+                }
         }).then(function(detalhes)
         {
             if(detalhes['cliente'] == null)
@@ -59,46 +59,42 @@ $("#pieChart").ready(function ()
 {
     var url = $('#pieChart').data('info');
 
-    var pieChartCanvas = $('#pieChart').get(0).getContext('2d')
-    var pieChart       = new Chart(pieChartCanvas)
+    var pieChartCanvas = $('#pieChart').get(0).getContext('2d');
+    var pieChart = new Chart(pieChartCanvas);
 
-    var pieOptions     = {
+    var pieData = [];
+
+    var pieOptions = {
         //Boolean - Whether we should show a stroke on each segment
-        segmentShowStroke    : true,
+        segmentShowStroke : true,
         //String - The colour of each segment stroke
-        segmentStrokeColor   : '#fff',
+        segmentStrokeColor : '#fff',
         //Number - The width of each segment stroke
-        segmentStrokeWidth   : 2,
+        segmentStrokeWidth : 2,
         //Number - The percentage of the chart that we cut out of the middle
-        percentageInnerCutout: 50, // This is 0 for Pie charts
+        percentageInnerCutout : 0, // This is 0 for Pie charts
         //Number - Amount of animation steps
-        animationSteps       : 100,
+        animationSteps : 20,
         //String - Animation easing effect
-        animationEasing      : 'easeOutBounce',
+        animationEasing : 'easeOutQuint',
         //Boolean - Whether we animate the rotation of the Doughnut
-        animateRotate        : true,
+        animateRotate : false,
         //Boolean - Whether we animate scaling the Doughnut from the centre
-        animateScale         : false,
+        animateScale : true,
         //Boolean - whether to make the chart responsive to window resizing
-        responsive           : true,
+        responsive : true,
         // Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
-        maintainAspectRatio  : true,
+        maintainAspectRatio : true,
         //String - A legend template
-        legendTemplate       : '<ul class="<%=name.toLowerCase()%>-legend"><% for (var i=0; i<segments.length; i++){%><li><span style="background-color:<%=segments[i].fillColor%>"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>'
-      }
-
-    var PieData = [];
-    
-    pieChart.Doughnut(PieData, pieOptions)
-
+        legendTemplate : '<ul class="<%=name.toLowerCase()%>-legend"><% for (var i=0; i<segments.length; i++){%><li><span style="background-color:<%=segments[i].fillColor%>"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>'
+      };
 
     $.ajax(url, {
         method: 'GET',
-        type: 'json',
+        type: 'json'
     }).done(function(data)
     {
-        
-        PieData = [
+        pieData = [
           {
             value    : data[0].brinquedos,
             color    : '#f56954',
@@ -141,70 +137,66 @@ $("#pieChart").ready(function ()
             highlight: '#1460a6',
             label    : 'Roupa'
           }
-        ]
+        ];
        
-        pieChart.Doughnut(PieData, pieOptions)
+        pieChart.Doughnut(pieData, pieOptions)
     })
-
-})
+});
 
 $("#areaChart").ready(function()
 {
     var url = $('#areaChart').data('info');
-    var areaChartCanvas = $('#areaChart').get(0).getContext('2d')
-    // This will get the first returned node in the jQuery collection.
-    var areaChart       = new Chart(areaChartCanvas)
+    var areaChartCanvas = $('#areaChart').get(0).getContext('2d');
 
-    var areaChartData = {}
+    // This will get the first returned node in the jQuery collection.
+    var areaChart = new Chart(areaChartCanvas);
+
+    var areaChartData = {};
 
     var areaChartOptions = {
       //Boolean - If we should show the scale at all
-      showScale               : true,
+      showScale : true,
       //Boolean - Whether grid lines are shown across the chart
-      scaleShowGridLines      : false,
+      scaleShowGridLines : false,
       //String - Colour of the grid lines
-      scaleGridLineColor      : 'rgba(0,0,0,.05)',
+      scaleGridLineColor : 'rgba(0,0,0,.05)',
       //Number - Width of the grid lines
-      scaleGridLineWidth      : 1,
+      scaleGridLineWidth : 1,
       //Boolean - Whether to show horizontal lines (except X axis)
       scaleShowHorizontalLines: true,
       //Boolean - Whether to show vertical lines (except Y axis)
-      scaleShowVerticalLines  : true,
+      scaleShowVerticalLines : true,
       //Boolean - Whether the line is curved between points
-      bezierCurve             : true,
+      bezierCurve : true,
       //Number - Tension of the bezier curve between points
-      bezierCurveTension      : 0.3,
+      bezierCurveTension : 0.3,
       //Boolean - Whether to show a dot for each point
-      pointDot                : false,
+      pointDot : false,
       //Number - Radius of each point dot in pixels
-      pointDotRadius          : 4,
+      pointDotRadius : 4,
       //Number - Pixel width of point dot stroke
-      pointDotStrokeWidth     : 1,
+      pointDotStrokeWidth : 1,
       //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
       pointHitDetectionRadius : 20,
       //Boolean - Whether to show a stroke for datasets
-      datasetStroke           : true,
+      datasetStroke : true,
       //Number - Pixel width of dataset stroke
-      datasetStrokeWidth      : 2,
+      datasetStrokeWidth : 2,
       //Boolean - Whether to fill the dataset with a color
-      datasetFill             : true,
+      datasetFill : true,
       //String - A legend template
-      legendTemplate          : '<ul class="<%=name.toLowerCase()%>-legend"><% for (var i=0; i<datasets.length; i++){%><li><span style="background-color:<%=datasets[i].lineColor%>"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>',
+      legendTemplate : '<ul class="<%=name.toLowerCase()%>-legend"><% for (var i=0; i<datasets.length; i++){%><li><span style="background-color:<%=datasets[i].lineColor%>"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>',
       //Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
-      maintainAspectRatio     : true,
+      maintainAspectRatio : true,
       //Boolean - whether to make the chart responsive to window resizing
-      responsive              : true
-    }
-
-    //Create the line chart
-    areaChart.Line(areaChartData, areaChartOptions)
+      responsive : true
+    };
 
     $.ajax(url, {
         method: 'GET',
-        type: 'json',
+        type: 'json'
     }).done(function(data)
     {
-        
         areaChartData = {
             labels  : [],
             datasets: [
@@ -219,7 +211,7 @@ $("#areaChart").ready(function()
                 data                : []
               }
             ]
-          }
+          };
 
         data.forEach(mes => {
             areaChartData.labels.push(mes.mes)
@@ -228,4 +220,4 @@ $("#areaChart").ready(function()
        
         areaChart.Line(areaChartData, areaChartOptions)
     })
-})
+});
