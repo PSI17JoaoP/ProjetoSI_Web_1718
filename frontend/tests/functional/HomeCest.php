71d26a819925ddae61c0e5a38d6d6c7c9582fb24
Leonardo <leonardo@MasterLeo>
2017-12-30 13:35:15 +0000

    Prep. para teste homepage

--- frontend/tests/functional/HomeCest.php
+++ frontend/tests/functional/HomeCest.php
@@ -3,15 +3,31 @@
 namespace frontend\tests\functional;
 
 use frontend\tests\FunctionalTester;
+use common\fixtures\UserFixture;
+
 
 class HomeCest
 {
+    public function _before(FunctionalTester $I)
+    {
+        $I->haveFixtures([
+            'user' => [
+                'class' => UserFixture::className(),
+                'dataFile' => codecept_data_dir() . 'login_data.php'
+            ]
+        ]);
+        $I->amOnRoute('site/login');
+
+        $I->submitForm('#login-form',  [
+            'LoginForm[username]' => 'erao',
+            'LoginForm[password]' => 'password_0',
+        ]);
+    }
+
     public function checkOpen(FunctionalTester $I)
     {
         $I->amOnPage(\Yii::$app->homeUrl);
-        $I->see('My Company');
-        $I->seeLink('About');
-        $I->click('About');
-        $I->see('This is the About page.');
+        $I->see('Sistema de Trocas');
+        
     }
 }
