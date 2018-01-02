6279ba96a6c3374c3db1e06465f9f83cb5cc0ece
Miguel Gameiro <miguel_gameiro15@hotmail.com>
2018-01-02 16:54:05 +0000

    historico+separadores

--- frontend/views/user/historico.php
+++ frontend/views/user/historico.php
@@ -4,93 +4,81 @@ use yii\helpers\Html;
 use yii\helpers\Url;
 use yii\bootstrap\ActiveForm;
 
+
 $this->title = 'Histórico';
 ?>
 
 <div class="col-12 col-md-8">
-    <div class="panel panel-default">
-        <div class="panel-body">
-            <div class="row" style="margin-bottom: 20px">
-                <div class="col-6 col-md-4">
-                    <a href="#" class="btn btn-primary">Anúncios</a>
-                </div>
-                <div class="col-6 col-md-4">
-                    <a href="#" class="btn btn-primary">Propostas</a>
-                </div>
-                <div class="col-6 col-md-4">
-                    <div class="btn-group">
-                        <a href="#" class="btn btn-primary">Ordenar por: </a>
-                        <a href="#" class="btn btn-primary dropdown-toggle" data-toggle="dropdown"><span class="caret"></span></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="#">Titulo</a></li>
-                            <li><a href="#">Data</a></li>
-                            <li><a href="#">Estado</a></li>
-                            
-                            
-                        </ul>
+
+        <div class="panel panel-default">
+            <div class="panel-body">
+
+
+                <ul class="nav nav-tabs">
+                <li class="nav-item">
+                    <a class="nav-link active" data-toggle="tab" href="#anuncios" style="">Anúncios</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" data-toggle="tab" href="#propostas" style="">Propostas</a>
+                </li>
+                
+                
+                </ul>
+                <div id="myTabContent" class="tab-content">
+                <div class="tab-pane active" id="anuncios">
+                <?php foreach($anuncios as $dados) { ?>
+                    
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="panel panel-default">
+                                <div class="panel-body">
+                                    <div class="row">
+                                        <div class="col-md-4">
+                                            <p style="margin-top: 12px; margin-left: 5px"><b>Anúncio:</b><?= Html::encode($dados["titulo"]) ?></p>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <p style="margin-top: 12px; margin-left: 5px"><b>Criado em:</b><?= Html::encode($dados["data_criacao"]) ?></p>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <p style="margin-top: 12px; margin-left: 5px"><b>Estado:</b><?= Html::encode($dados["estado"]) ?></p>
+                                        </div>                                                                                
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    
                     </div>
+                <?php } ?>
                 </div>
-            </div>
+                <div class="tab-pane fade" id="propostas">
+                <?php foreach($propostas as $dados) { ?>
 
-            
-
-            <div class="panel panel-default">
-                <div class="panel-body">
-
-                    <!--Código para apresentar os anuncios-->                      
-                    
-                    <?php foreach($anuncios as $dados) { ?>
-                        <div class="row">
-                            <div class="col-md-12">
-                                <div class="panel panel-default">
-                                    <div class="panel-body">
-                                        <div class="row">
-                                            <div class="col-md-4">
-                                                <p style="margin-top: 12px; margin-left: 5px"><b>Anúncio:</b><?= Html::encode($dados["titulo"]) ?></p>
-                                            </div>
-                                            <div class="col-md-4">
-                                                <p style="margin-top: 12px; margin-left: 5px"><b>Criado em:</b><?= Html::encode($dados["data_criacao"]) ?></p>
-                                            </div>
-                                            <div class="col-md-4">
-                                                <p style="margin-top: 12px; margin-left: 5px"><b>Estado:</b><?= Html::encode($dados["estado"]) ?></p>
-                                            </div>                                                                                
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="panel panel-default">
+                                <div class="panel-body">
+                                    <div class="row">
+                                        <div class="col-md-4">
+                                            <p style="margin-top: 12px; margin-left: 5px"><b>Proposta:</b><?= Html::encode($dados[1][0]->nome) ?></p>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <p style="margin-top: 12px; margin-left: 5px"><b>Proposto em:</b><?= Html::encode($dados[0]->data_proposta) ?></p>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <p style="margin-top: 12px; margin-left: 5px"><b>Estado:</b><?= Html::encode($dados[0]->estado) ?></p>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    <?php } ?> 
-                    
-                    <!--Código para apresentar as propostas-->  
+                        </div>              
+                    </div>
 
-                    <?php foreach($propostas as $dados) { ?>
-
-                        <div class="row">
-                            <div class="col-md-12">
-                                <div class="panel panel-default">
-                                    <div class="panel-body">
-                                        <div class="row">
-                                            <div class="col-md-4">
-                                                <p style="margin-top: 12px; margin-left: 5px"><b>Proposta:</b><?= Html::encode($dados[1][0]->nome) ?></p>
-                                            </div>
-                                            <div class="col-md-4">
-                                                <p style="margin-top: 12px; margin-left: 5px"><b>Proposto em:</b><?= Html::encode($dados[0]->data_proposta) ?></p>
-                                            </div>
-                                            <div class="col-md-4">
-                                                <p style="margin-top: 12px; margin-left: 5px"><b>Estado:</b><?= Html::encode($dados[0]->estado) ?></p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
-                    <?php } ?>                        
-
-                    
-                </div>
+                <?php } ?>
             </div>
-
+                
+                 
         </div>
     </div>
+
+     
 </div>
