fca5022bd9154c8929c7435b8e9c1af5849b3a78
Miguel Gameiro <miguel_gameiro15@hotmail.com>
2018-01-02 17:08:42 +0000

    fix

--- frontend/views/user/historico.php
+++ frontend/views/user/historico.php
@@ -2,7 +2,7 @@
 /* @var $this yii\web\View */
 use yii\helpers\Html;
 use yii\helpers\Url;
-use yii\bootstrap\ActiveForm;
+
 
 
 $this->title = 'Histórico';
@@ -78,7 +78,7 @@ $this->title = 'Histórico';
                 
                  
         </div>
-    </div>
-
+        </div>
+        </div>
      
 </div>
