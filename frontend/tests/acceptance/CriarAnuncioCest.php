215f23f786b9f1550548b5a25efa912d0b65ff26
João P <2160453@my.ipleiria.pt>
2018-01-14 22:38:16 +0000

    Progresso

--- frontend/tests/acceptance/CriarAnuncioCest.php
+++ frontend/tests/acceptance/CriarAnuncioCest.php
@@ -17,7 +17,7 @@ class CriarAnuncioCest
 
     public function checkCriarAnuncioReceber(AcceptanceTester $I)
     {
-        $I->amOnPage('site/login');
+        $I->amOnPage('/site/login');
 
         $I->submitForm('#login-form',  [
             'LoginForm[username]' => 'erao',
@@ -37,6 +37,7 @@ class CriarAnuncioCest
 
         $I->click('#field-cat-oferta');
         $I->see('Eletrónica');
+        $I->click('Eletrónica');
 
         $I->wait(3);
 
@@ -55,6 +56,7 @@ class CriarAnuncioCest
 
         $I->click('#field-cat-procura');
         $I->see('Livros');
+        $I->click('Livros');
 
         $I->wait(3);
 
@@ -93,34 +95,59 @@ class CriarAnuncioCest
 
     /*public function checkCriarAnuncioTodos(FunctionalTester $I)
     {
-        $I->amOnPage('/');
-        $I->see('Sistema de Trocas');
+        $I->amOnPage('site/login');
+
+        $I->submitForm('#login-form',  [
+            'LoginForm[username]' => 'erao',
+            'LoginForm[password]' => 'password_0',
+        ]);
+
+        $I->wait(5);
 
         $I->see('Criar Anúncio');
         $I->click('Criar Anúncio');
 
+        $I->wait(3);
+
         $I->see('Título');
         $I->fillField(['name' => 'AnuncioForm[titulo]'], 'TesteFuncional');
+        $I->wait(1);
 
         $I->click('#field-cat-oferta');
-        $I->click('Eletrónica');
         $I->see('Eletrónica');
+        $I->click('Eletrónica');
 
-        $I->fillField('AnuncioForm[quantOferta]', '3');
-        $I->fillField('EletronicaForm[0][nome]', 'TesteFuncional');
-        $I->fillField('EletronicaForm[0][marca]', 'TesteFuncional');
-        $I->fillField('EletronicaForm[0][descricao]', 'TesteFuncional');
+        $I->wait(3);
+
+        $I->seeElement('EletronicaForm[0][nome]');
+        $I->seeElement('EletronicaForm[0][marca]');
+        $I->seeElement('EletronicaForm[0][descricao]');
+
+        $I->fillField(['name' => 'AnuncioForm[quantOferta]'], '3');
+        $I->wait(1);
+        $I->fillField(['name' => 'EletronicaForm[0][nome]'], 'TesteFuncional');
+        $I->wait(1);
+        $I->fillField(['name' => 'EletronicaForm[0][marca]'], 'TesteFuncional');
+        $I->wait(1);
+        $I->fillField(['name' => 'EletronicaForm[0][descricao]'], 'TesteFuncional');
+        $I->wait(1);
 
         $I->click('#field-cat-procura');
-        $I->click('Todos');
         $I->see('Todos');
+        $I->click('Todos');
 
-        $I->fillField('AnuncioForm[comentarios]', 'ComentariosTeste');
+        $I->wait(3);
 
-        $I->attachFile('AnuncioForm[imageFiles]', 'image.jpg');
+        $I->fillField(['name' => 'AnuncioForm[comentarios]'], 'ComentariosTeste');
+        $I->wait(1);
+
+        $I->attachFile(['name' => 'AnuncioForm[imageFiles]'], 'image.jpg');
+        $I->wait(5);
 
         $I->see('Concluido');
         $I->click('Concluido');
+
+        $I->wait(5);
 
         $I->see('Sucesso! O seu anúncio foi criado com sucesso.');
         $I->see('Título:TesteFuncional');
