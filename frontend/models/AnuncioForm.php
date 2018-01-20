f438e0a5bc9b2497102ea8003c532be5c610b46e
Leonardo <leonardo@MasterLeo>
2018-01-20 15:45:01 +0000

    + const

--- frontend/models/AnuncioForm.php
+++ frontend/models/AnuncioForm.php
@@ -30,7 +30,8 @@ class AnuncioForm extends Model
 
     //Constantes de Estado do Anúncio
     //const ESTADO_ATIVO = 'ATIVO';
-    //const ESTADO_FECHADO = 'CONCLUIDO';
+    //const ESTADO_CONCLUIDO = 'CONCLUIDO';
+    //const ESTADO_FECHADO = 'FECHADO';
 
     /**
      * @inheritdoc
